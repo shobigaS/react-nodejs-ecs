1d5955aec85338366f23e2e609f3a8631a7133fd
shobiga <shobiga.ephronsystem@gmail.com>
2021-01-08 11:40:44 +0530

    Updates

---
+++ precache-manifest.5e1e8cc15cf44ab42571dd110820655e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "057db31880cfbc1a72397769cdebc896",
+    "url": "/react-nodejs-ecs/index.html"
+  },
+  {
+    "revision": "5009b8f8302e304bd12f",
+    "url": "/react-nodejs-ecs/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "112d2ffe0380d9cdb1a2",
+    "url": "/react-nodejs-ecs/static/css/main.7071ae30.chunk.css"
+  },
+  {
+    "revision": "5009b8f8302e304bd12f",
+    "url": "/react-nodejs-ecs/static/js/2.28dc267f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-nodejs-ecs/static/js/2.28dc267f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "112d2ffe0380d9cdb1a2",
+    "url": "/react-nodejs-ecs/static/js/main.5d4f1228.chunk.js"
+  },
+  {
+    "revision": "98e098eb660873e87462",
+    "url": "/react-nodejs-ecs/static/js/runtime-main.7610f841.js"
+  }
+]);
