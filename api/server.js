6659f34f216e162f9ea5542fce6a6181467d780d
shobiga <shobiga.ephronsystem@gmail.com>
2021-01-08 11:33:03 +0530

    first commit

--- api/server.js
+++ api/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express(),
-      bodyParser = require("body-parser");
-      port = 3080;
+    bodyParser = require("body-parser");
+port = 3080;
 
 // place holder for the data
 const users = [];
@@ -11,19 +11,19 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../my-app/build')));
 
 app.get('/api/users', (req, res) => {
-  console.log('api/users called!')
-  res.json(users);
+    console.log('api/users called!')
+    res.json(users);
 });
 
 app.post('/api/user', (req, res) => {
-  const user = req.body.user;
-  console.log('Adding user:::::', user);
-  users.push(user);
-  res.json("user addedd");
+    const user = req.body.user;
+    console.log('Adding user:::::', user);
+    users.push(user);
+    res.json("user addedd");
 });
 
-app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname, '../my-app/build/index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../my-app/build/index.html'));
 });
 
 app.listen(port, () => {
